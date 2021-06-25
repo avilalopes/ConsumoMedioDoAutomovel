affbc168293abcc0b84ed81b4e67ca6215873526
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-06-25 20:26:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            int distancia;
+            double combustivelGasto, consumoMedio;
+
+            Console.WriteLine("Informe a distância percorrida: ");
+            distancia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de combustível gasto: ");            
+            combustivelGasto = Convert.ToDouble(Console.ReadLine());
+
+            consumoMedio = distancia/combustivelGasto; //atribua a formula para que o codigo funcione corretamente
+
+            Console.WriteLine("{0:0.000} km/l", consumoMedio);
+
+    }
+
+}
